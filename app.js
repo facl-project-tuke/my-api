f654465c03d39510336f072d4f3045a7d58de195
facl-project-tuke <vanya.k005@gmail.com>
2024-05-12 23:42:35 +0200

    asdas

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { Pool } = require('pg');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Параметры подключения к базе данных
+const pool = new Pool({
+  user: 'admin',
+  host: '147.232.24.160',
+  database: 'face_detector_test',
+  password: 'root',
+  port: 5432,
+});
+
+// Маршрут для получения всех таблиц
+app.get('/tables', async (req, res) => {
+    try {
+      const query = "SELECT table_name FROM information_schema.tables WHERE table_schema='public';";
+      const { rows } = await pool.query(query);
+      res.json(rows);
+    } catch (error) {
+      console.error(error);  // Добавлено логирование ошибки в консоль
+      res.status(500).json({ error: error.message });
+    }
+  });
+  
+
+app.listen(port, () => {
+  console.log(`Сервер запущен на http://localhost:${port}`);
+});
+
